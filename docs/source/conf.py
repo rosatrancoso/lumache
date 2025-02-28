7e132900aca211b891c88814532cd068fcb01cc8
Rosa Trancoso <rosatrancoso@gmail.com>
2025-02-28 23:37:20 +1300

    use sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,5 +39,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
-html_theme = 'furo'
+#html_theme = 'furo'
+#html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
